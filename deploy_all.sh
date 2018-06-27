5cc1f31b4c03d6d0ba0766ff54b2dfc8d917cb07
antweiss <anton@otomato.link>
2018-06-27 03:24:39 -0500

    versioning for v03

--- deploy_all.sh
+++ deploy_all.sh
@@ -1,2 +1,2 @@
 #! /bin/bash -f
-kubectl apply -f aleph-dep.yml  -f front-dep.yml -f beth-dep.yml -n games
+kubectl apply -f aleph-dep.yml  -f front-dep.yml -f beth-dep.yml -f aleph-dep1.yml -n games
