0a833d8734b3a1d41bd2f59e9460df20dccca547
Anton Weiss <anton@otomato.link>
2018-09-27 13:04:38 +0300

    deploy loader with apps

--- deployments/deploy_all.sh
+++ deployments/deploy_all.sh
@@ -1,4 +1,4 @@
 #! /bin/bash -f
 kubectl create ns games
 kubectl label ns games istio-injection=enabled
-kubectl apply -f aleph-dep.yml  -f front-dep.yml -f beth-dep.yml -n games
+kubectl apply -f aleph-dep.yml  -f front-dep.yml -f beth-dep.yml -f loader-dep.yml -n games
