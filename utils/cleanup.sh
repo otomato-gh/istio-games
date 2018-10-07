ec51a09707072d7ac452f11e1389a6d9d10d88e9
Anton Weiss <anton@otomato.link>
2018-10-07 11:46:20 +0300

    Cleanup mixer and servicgraph

--- utils/cleanup.sh
+++ utils/cleanup.sh
@@ -3,3 +3,6 @@ kubectl delete ns games
 #kubectl delete crd birdwatches.otomato.link
 #kubectl delete deploy birdwatch
 kubectl delete pod -n istio-system -l app=istio-ingressgateway
+kubectl delete pod -n istio-system -l app=telemetry
+kubectl delete pod -n istio-system -l app=servicegraph
+
